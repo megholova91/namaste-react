4907c4c1f67278935845281279b242b06ff6ec2d
Megholova Mukherjee <megholova@gmail.com>
2024-03-09 19:40:38 +0530

    episode 05 part 2

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,9 @@
 import RestaurantContainer from "./RestaurantContainer";
-import Search from "./Search";
+
 
 const Body = () => {
   return (
     <div className="body">
-      <Search />
       <RestaurantContainer />
     </div>
   );
--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -1,13 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import { useState } from "react";
+import resList from '../utils/mockData'
 
 const RestaurantContainer = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  //filter restaurants with 4+ rating
+  const handleClick = () => {
+    const filteredListOfRestaurants = listOfRestaurants.filter(res => res.data.avgRating > 4)
+    setListOfRestaurants(filteredListOfRestaurants);
+  }
   return (
-    <div className="res-container">
-      {resList.map((resData) => (
-        <RestaurantCard key={resData.data.id} resData={resData} />
-      ))}
-    </div>
+    <>
+      <div className="filter">
+        <button className="filter-btn" onClick={handleClick}>Top Rated Restaurants</button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((resData) => (
+          <RestaurantCard key={resData.data.id} resData={resData} />
+        ))}
+      </div>
+    </>
   );
 };
 
